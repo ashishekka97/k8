a4c86ddd25bb3a39f89300fa4a7f5f42295e3332
Ashish Ekka <ashishekka329@gmail.com>
2023-10-20 22:47:51 +0530

    Improve desktop functionalty

---
+++ common/src/commonMain/kotlin/me/ashishekka/k8/configs/ColorScheme.kt
@@ -0,0 +1,23 @@
+package me.ashishekka.k8.data
+
+enum class ColorScheme(
+    val schemeName: String,
+    val background: Long,
+    val foreground: Long
+) {
+    DEFAULT("default", background = 0xFF495054, foreground = 0xFFe3e8ea),
+    TERMINAL("terminal", background = 0xFFB00020, 0xFF00ff00),
+    GAMEBOY("gameboy", background = 0xff9bbc0f, foreground = 0xFF0f380f),
+    ICE("ice", background = 0xff365673, foreground = 0xFFcfe2f3),
+    KHAKI("khaki", background = 0xff6f7769, foreground = 0xFFd5ddcf),
+    C64("c64", background = 0xff36209b, foreground = 0xFF7664d9);
+
+    companion object {
+        fun getAllThemes() = values().map { it.schemeName }
+        fun getThemeFromIndex(index: Int) = try {
+            values()[index]
+        } catch (ex: Throwable) {
+            DEFAULT
+        }
+    }
+}
