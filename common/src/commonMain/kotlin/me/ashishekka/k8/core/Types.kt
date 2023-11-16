9a27aab4ea5416421bd10023a6c2bf5131456724
Ashish Ekka <ashishekka329@gmail.com>
2023-11-17 01:13:35 +0530

    Add support for Super Chip

--- common/src/commonMain/kotlin/me/ashishekka/k8/core/Types.kt
+++ common/src/commonMain/kotlin/me/ashishekka/k8/core/Types.kt
@@ -1,6 +1,5 @@
 package me.ashishekka.k8.core
 
-
 typealias ByteStore = UByte
 typealias WordStore = UShort
 typealias Memory = Array<ByteStore>
@@ -16,9 +15,13 @@ fun Triple<ByteStore, ByteStore, ByteStore>.y() = second
 
 fun Triple<ByteStore, ByteStore, ByteStore>.n() = third
 
-fun Triple<ByteStore, ByteStore, ByteStore>.toByteStore() = ((y().toUInt() shl 4) or n().toUInt()).toUByte()
+fun Triple<ByteStore, ByteStore, ByteStore>.toByteStore(): ByteStore {
+    return ((y().toUInt() shl 4) or n().toUInt()).toUByte()
+}
 
-fun Triple<ByteStore, ByteStore, ByteStore>.toInt() = (x().toInt() shl 8) or (y().toInt() shl 4) or n().toInt()
+fun Triple<ByteStore, ByteStore, ByteStore>.toInt(): Int {
+    return (x().toInt() shl 8) or (y().toInt() shl 4) or n().toInt()
+}
 
 fun Triple<ByteStore, ByteStore, ByteStore>.toWordStore() = toInt().toUShort()
 
@@ -55,6 +58,54 @@ fun VideoMemory.clear() {
     forEachIndexed { y, row ->
         row.forEachIndexed { x, _ ->
             this[y][x] = false
+        }
+    }
+}
+
+fun VideoMemory.set(x: Int, y: Int, value: Boolean, resolution: Resolution) {
+    if (resolution == Resolution.HIGH) {
+        this[y][x] = value
+    } else {
+        if (2 * y < size && 2 * x < this[0].size) {
+            this[2 * y][2 * x] = value
+            this[2 * y + 1][2 * x] = value
+            this[2 * y][2 * x + 1] = value
+            this[2 * y + 1][2 * x + 1] = value
+        }
+    }
+}
+
+fun VideoMemory.get(x: Int, y: Int, resolution: Resolution): Boolean {
+    return if (resolution == Resolution.HIGH) {
+        this[y][x]
+    } else {
+        if (2 * y < size && 2 * x < this[0].size) {
+            this[2 * y][2 * x] and
+                this[2 * y + 1][2 * x] and
+                this[2 * y][2 * x + 1] and
+                this[2 * y + 1][2 * x + 1]
+        } else {
+            false
+        }
+    }
+}
+
+fun VideoMemory.setRow(row: Int, data: BooleanArray, resolution: Resolution) {
+    if (resolution == Resolution.HIGH) {
+        this[row] = data
+    }
+}
+
+fun VideoMemory.getRow(row: Int, resolution: Resolution): BooleanArray {
+    return if (resolution == Resolution.HIGH) {
+        this[row]
+    } else {
+        if (row * 2 < size) {
+            this[row * 2].mapIndexed { index, b ->
+                b and this[row * 2 + 1][index]
+            }.toBooleanArray()
+        } else {
+            BooleanArray(128)
         }
     }
 }
